ab930afbdf23f6eabd61c417ac5b9de3639bd44f
Tri Pham <K21003770@kcl.ac.uk>
2022-10-10 02:19:17 +0100

    task completed

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes <= 0):
+        raise ValueError
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        prime = True
+        for x in range(2,num//2 + 1):
+            if num % x == 0:
+                prime = False
+        if prime:
+            list.append(num)
+        num += 1
     return list
